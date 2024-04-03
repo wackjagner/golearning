2174bf58d7f08e01020563e9db433c1b7e0adbb8
wackjagner <jack.wagner1997@gmail.com>
2024-04-03 14:08:37 -0700

    Added error handling.

--- greetings.go
+++ greetings.go
@@ -1,10 +1,18 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // Hello returns a greeting for the named person
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+	//Return error if no name given
+	if name == "" {
+		return "", errors.New("You forgot to provide a name.")
+	}
+
 	//Return a greeting that embeds the name in a message
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message
+	return message, nil
 }
